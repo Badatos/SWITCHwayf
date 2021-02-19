22ee7c15abddf7fbb6a1b723a4e2304d2629ca5d
Geoffroy Arnoud <geoffroy.arnoud@renater.fr>
2021-02-19 16:12:18 +0100

    Test to check edugain perfs

---
+++ test/edugainIdpApiTest.php
@@ -0,0 +1,78 @@
+<?php // Copyright (c) 2019 Geoffroy Arnoud, Guillaume Rousse, and SWITCHwayf contributors
+
+use PHPUnit\Framework\TestCase;
+
+require("../lib/idpApiObjects.php");
+//sortIdentityProviders($IDProviders);
+
+function time_elapsed($message)
+{
+    static $last = null;
+
+    $now = microtime(true);
+
+    if ($last != null) {
+        printf("%s => %f\n", $message, $now - $last);
+    }
+
+    $last = $now;
+}
+
+final class IdpApiTest extends TestCase
+{
+    public function testSort()
+    {
+        time_elapsed("[Sort] Init");
+
+        require("edugain-IDProvider.metadata.php");
+        $IDProviders = $metadataIDProviders;
+
+        time_elapsed("[Sort] doSort");
+
+        sortIdentityProviders($IDProviders);
+
+        time_elapsed("[Sort] endSort");
+    }
+
+    // public function testGetFirstPage()
+    // {
+    //     time_elapsed("[GetFirstPage] init");
+    //
+    //     require("edugain-IDProvider.metadata.php");
+    //
+    //     time_elapsed("[GetFirstPage] Loading eduGain IDP");
+    //
+    //     $repo = new IdpRepository($metadataIDProviders);
+    //
+    //     time_elapsed("[GetFirstPage] Creating repository");
+    //
+    //     file_put_contents("/tmp/eduGainFirstPage.json", $repo->toJsonByPage(1, 100));
+    //
+    //     time_elapsed("[GetFirstPage] Putting 1st page to a file");
+    // }
+    //
+    //
+    // public function testQuery()
+    // {
+    //     time_elapsed("[Query] init");
+    //     global $IDProviders;
+    //
+    //
+    //     require("edugain-IDProvider.metadata.php");
+    //
+    //     $IDProviders = $metadataIDProviders;
+    //
+    //     time_elapsed("[Query] Loading eduGain IDP");
+    //
+    //     $repo = new IdpRepository($metadataIDProviders);
+    //
+    //     time_elapsed("[Query] Creating repository");
+    //
+    //     file_put_contents(
+    //       "/tmp/eduGainFirstPageQuery.json",
+    //       $repo->toJsonByQuery("renater", 1, 100)
+    //     );
+    //
+    //     time_elapsed("[Query] Putting 1st page to a file");
+    // }
+}
